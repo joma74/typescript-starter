cefb50982e3194a9be034f65252f6b72ff21daa4
joma74 <dev-mails@gmx.net>
2016-12-06 01:03:27 +0100

    Relational quirk tests

--- src/test/quirks/Jsonquirks.spec.ts
+++ src/test/quirks/Jsonquirks.spec.ts
@@ -1,4 +1,4 @@
-var assert = require('assert')
+var assert = require('assert');
 var fs = require('fs');
 
 describe('Jsonquirks', () => {
---
+++ src/test/quirks/RelationalQuirks.ts
@@ -0,0 +1,21 @@
+var assert = require('assert');
+
+describe('Relationalquirks', () => {
+    it('#1 a < b forces toNumber if any side is a number', () => {
+        let a :any = [42];
+        let b :any = ["43"];
+        assert.ok(a < b);
+    });
+    it('#2 a < b forces lexicographic ToPrimitive if both side are a string', () => {
+        let a :any = ["42"];
+        let b :any = ["043"];
+        assert.ok(a > b);
+    });
+    it('#3 Yikes, a <= b does !(b < a)', () => {
+        let a : any = { b: 42 };
+        let b :any = { b: 43 };
+        assert.ok(a != b);
+        assert.ok(a <= b);
+        assert.ok(a >= b);
+    });
+});
